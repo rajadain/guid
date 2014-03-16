46db82dc5a28943367a3e9b110bf4c10ebe0d5b1
Terence Tuhinanshu <terence@tuhinanshu.com>
2014-03-16 18:15:44 -0400

    Return {guid: <value>} object instead of raw value

--- js/guid.js
+++ js/guid.js
@@ -7,10 +7,10 @@ function guid(n) {
     var guids = [];
     
     for (i = 0; i < n; i++) {
-        guids.push('xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        guids.push({guid: 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
             var r = Math.random()*16|0, v = c === 'x' ? r : (r&0x3|0x8);
             return v.toString(16);
-        }));
+        })});
     }
     
     return guids;
