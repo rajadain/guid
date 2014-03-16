a0ac041635d743ebc8e460a59507038ed8fae1ee
Terence Tuhinanshu <terence@tuhinanshu.com>
2014-03-16 18:18:47 -0400

    Add master Backbone app file

---
+++ js/app.js
@@ -0,0 +1,92 @@
+$(function(){
+  var Guid = Backbone.Model.extend({
+    defaults: function() {
+      return {
+        guid: "00000000-0000-4000-0000-000000000000"
+      };
+    },
+    getDisplay: function(options) {
+      var displayValue = this.get('guid');
+
+      if (options.bUppercase) displayValue = displayValue.toUpperCase();
+      if (!options.bDashes) displayValue = displayValue.replace(/-/g,'');
+      if (options.bBraces) displayValue = '{' + displayValue + '}';
+      if (options.bCommas) displayValue = displayValue + ',';
+
+      return displayValue;
+    }
+  });
+
+  var GuidList = Backbone.Collection.extend({
+    model: Guid,
+    copy: function(options) {
+        return this.models.reduce(function(memo, guid) {
+          return memo + guid.getDisplay(options) + "\n";
+        }, '');
+    }
+  });
+  
+  var Guids = new GuidList;
+  
+  var GuidView = Backbone.View.extend({
+    tagName: 'li',
+    render: function(options) {
+      this.$el.html(this.model.getDisplay(options));
+      return this;
+    }
+  });
+
+  var AppView = Backbone.View.extend({
+    el: $('#guid-app'),
+
+    nQuantity: $('#nQuantity'),
+    bUppercase: $('#bUppercase'),
+    bDashes: $('#bDashes'),
+    bBraces: $('#bBraces'),
+    bCommas: $('#bCommas'),
+    
+    options: function() {
+      return {
+        bUppercase: this.bUppercase.prop('checked'),
+        bDashes: this.bDashes.prop('checked'),
+        bBraces: this.bBraces.prop('checked'),
+        bCommas: this.bCommas.prop('checked')
+      };
+    },
+
+    events: {
+      'click #aGenerate': 'generate',
+      'click #bUppercase': 'render',
+      'click #bDashes': 'render',
+      'click #bBraces': 'render',
+      'click #bCommas': 'render',
+      'click #aCopy': 'copy'
+    },
+
+    initialize: function() {
+      this.listenTo(Guids, 'reset', this.render);
+    },
+
+    generate: function() {
+      var numberOfGuids = parseInt($('#nQuantity').val());
+      Guids.reset(guid(numberOfGuids));
+    },
+
+    render: function() {
+
+      var guidList = this.$('#guid-list');
+      guidList.empty();
+      Guids.each(function(guid) {
+        var view = new GuidView({model: guid});
+        guidList.append(view.render(this.options()).el);
+      }, this);
+    },
+
+    copy: function() {
+      var guids = Guids.copy(this.options());
+      console.log(guids);
+    }
+  });
+
+  var App = new AppView;
+});
