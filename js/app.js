e3abfdb7c86af35e3f268ef8adafc21c135098d0
Terence Tuhinanshu <terence@tuhinanshu.com>
2014-03-22 17:03:19 -0400

    Standardize to four-space code alignment

--- js/app.js
+++ js/app.js
@@ -1,112 +1,112 @@
 $(function(){
-  var Guid = Backbone.Model.extend({
-    defaults: function() {
-      return {
-        guid: "00000000-0000-4000-0000-000000000000"
-      };
-    },
-    getDisplay: function(options) {
-      var displayValue = this.get('guid');
+    var Guid = Backbone.Model.extend({
+        defaults: function() {
+            return {
+                guid: "00000000-0000-4000-0000-000000000000"
+            };
+        },
+        getDisplay: function(options) {
+            var displayValue = this.get('guid');
 
-      if (options.bUppercase) displayValue = displayValue.toUpperCase();
-      if (!options.bDashes) displayValue = displayValue.replace(/-/g,'');
-      if (options.bBraces) displayValue = '{' + displayValue + '}';
-      if (options.bCommas) displayValue = displayValue + ',';
+            if (options.bUppercase) displayValue = displayValue.toUpperCase();
+            if (!options.bDashes) displayValue = displayValue.replace(/-/g,'');
+            if (options.bBraces) displayValue = '{' + displayValue + '}';
+            if (options.bCommas) displayValue = displayValue + ',';
 
-      return displayValue;
-    }
-  });
+            return displayValue;
+        }
+    });
 
-  var GuidList = Backbone.Collection.extend({
-    model: Guid,
-    seperator: "\n",
-    copy: function(options) {
-        var result = [];
-        this.models.map(function(guid) {
-          return result.push(guid.getDisplay(options));
-        });
-        return result.join(this.seperator);
-    }
-  });
-  
-  var Guids = new GuidList;
-  
-  var GuidView = Backbone.View.extend({
-    tagName: 'li',
-    render: function(options) {
-      this.$el.html(this.model.getDisplay(options));
-      return this;
-    }
-  });
-
-  var AppView = Backbone.View.extend({
-    el: $('#guid-app'),
-
-    nQuantity: $('#nQuantity'),
-    bUppercase: $('#bUppercase'),
-    bDashes: $('#bDashes'),
-    bBraces: $('#bBraces'),
-    bCommas: $('#bCommas'),
-    aCopy: $('#aCopy'),
+    var GuidList = Backbone.Collection.extend({
+        model: Guid,
+        seperator: "\n",
+        copy: function(options) {
+                var result = [];
+                this.models.map(function(guid) {
+                    return result.push(guid.getDisplay(options));
+                });
+                return result.join(this.seperator);
+        }
+    });
     
-    options: {
-      bUppercase: false,
-      bDashes: true,
-      bBraces: false,
-      bCommas: false
-    },
+    var Guids = new GuidList;
+    
+    var GuidView = Backbone.View.extend({
+        tagName: 'li',
+        render: function(options) {
+            this.$el.html(this.model.getDisplay(options));
+            return this;
+        }
+    });
 
-    events: {
-      'click #aGenerate': 'generate',
-      'click #bUppercase': 'render',
-      'click #bDashes': 'render',
-      'click #bBraces': 'render',
-      'click #bCommas': 'render'
-    },
+    var AppView = Backbone.View.extend({
+        el: $('#guid-app'),
 
-    initialize: function() {
-      this.listenTo(Guids, 'reset', this.render);
-      this.setOptions();
+        nQuantity: $('#nQuantity'),
+        bUppercase: $('#bUppercase'),
+        bDashes: $('#bDashes'),
+        bBraces: $('#bBraces'),
+        bCommas: $('#bCommas'),
+        aCopy: $('#aCopy'),
+        
+        options: {
+            bUppercase: false,
+            bDashes: true,
+            bBraces: false,
+            bCommas: false
+        },
 
-      ZeroClipboard.config({moviePath: 'js/ZeroClipboard.swf'});
-      this.clipper = new ZeroClipboard(aCopy);
-      this.clipper.on('load', function(clipper) {
-        clipper.on('complete', function(clipper) {
-          $('#aCopy').prop('disabled', true).html('<span class="glyphicon glyphicon-ok"></span> Copied')
-        });
-      });
+        events: {
+            'click #aGenerate': 'generate',
+            'click #bUppercase': 'render',
+            'click #bDashes': 'render',
+            'click #bBraces': 'render',
+            'click #bCommas': 'render'
+        },
 
-      this.generate();
-    },
+        initialize: function() {
+            this.listenTo(Guids, 'reset', this.render);
+            this.setOptions();
 
-    generate: function() {
-      var numberOfGuids = parseInt($('#nQuantity').val());
-      Guids.reset(guid(numberOfGuids));
-    },
+            ZeroClipboard.config({moviePath: 'js/ZeroClipboard.swf'});
+            this.clipper = new ZeroClipboard(aCopy);
+            this.clipper.on('load', function(clipper) {
+                clipper.on('complete', function(clipper) {
+                    $('#aCopy').prop('disabled', true).html('<span class="glyphicon glyphicon-ok"></span> Copied')
+                });
+            });
 
-    render: function() {
-      this.setOptions();
+            this.generate();
+        },
 
-      var guidList = this.$('#guid-list');
-      guidList.empty();
-      Guids.each(function(guid) {
-        var view = new GuidView({model: guid});
-        guidList.append(view.render(this.options).el);
-      }, this);
+        generate: function() {
+            var numberOfGuids = parseInt($('#nQuantity').val());
+            Guids.reset(guid(numberOfGuids));
+        },
 
-      this.clipper.setText(Guids.copy(this.options));
-      this.aCopy.removeProp('disabled').html('<span class="glyphicon glyphicon-export"></span> Copy</button>');
-    },
+        render: function() {
+            this.setOptions();
 
-    setOptions: function() {
-      this.options = {
-        bUppercase: this.bUppercase.prop('checked'),
-        bDashes: this.bDashes.prop('checked'),
-        bBraces: this.bBraces.prop('checked'),
-        bCommas: this.bCommas.prop('checked')
-      };
-    }
-  });
+            var guidList = this.$('#guid-list');
+            guidList.empty();
+            Guids.each(function(guid) {
+                var view = new GuidView({model: guid});
+                guidList.append(view.render(this.options).el);
+            }, this);
 
-  var App = new AppView;
+            this.clipper.setText(Guids.copy(this.options));
+            this.aCopy.removeProp('disabled').html('<span class="glyphicon glyphicon-export"></span> Copy</button>');
+        },
+
+        setOptions: function() {
+            this.options = {
+                bUppercase: this.bUppercase.prop('checked'),
+                bDashes: this.bDashes.prop('checked'),
+                bBraces: this.bBraces.prop('checked'),
+                bCommas: this.bCommas.prop('checked')
+            };
+        }
+    });
+
+    var App = new AppView;
 });
